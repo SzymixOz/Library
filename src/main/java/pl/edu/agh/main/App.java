2495437109c7d19949ce07a63725e4e914333efb
Marcin Chudy <412344@student.agh.edu.pl>
2023-12-02 19:20:24 +0100

    Packages refactor

--- src/main/java/pl/edu/agh/main/App.java
+++ src/main/java/pl/edu/agh/main/App.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.to2.example;
+package pl.edu.agh.main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
--- src/test/java/pl/edu/agh/main/MainTest.java
+++ src/test/java/pl/edu/agh/main/MainTest.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.to2.example;
+package pl.edu.agh.main;
 
 import org.junit.jupiter.api.Test;
 
